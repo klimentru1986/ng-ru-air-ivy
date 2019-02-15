7e366de752852413b5926578ae6a62806792beb5
klimentru1986 <kliment1986@inbox.ru>
2019-02-15 22:52:57 +0300

    multy apps

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import { ɵrenderComponent as renderComponent } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { HelloWorldComponent } from './new-app/hello-world/hello-world.component';
 
 renderComponent(AppComponent);
+
+renderComponent(HelloWorldComponent);
+
